c48065e4821f6e14f9afd94ceed0db0b72bad12d
Jakub Szwachła <meszkax@gmail.com>
2025-12-14 21:12:50 +0100

    Refactor main loop

--- src/main/scala/pkg/Main.scala
+++ src/main/scala/pkg/Main.scala
@@ -3,7 +3,7 @@ import com.outworkers.phantom.dsl.{CreateQueryOps, context}
 import java.time.Duration
 import java.util.Properties
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.nibor.autolink.{LinkExtractor, LinkType}
 import sttp.client4.{Backend, DefaultFutureBackend, Response, UriContext, quickRequest}
@@ -74,26 +74,67 @@ object Main extends MyDBProvider {
     }
   }
 
+  val bootstrapServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+
+  val consumerProps = new Properties()
+  consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+  consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "sms-phishing-filter-consumer-group")
+  consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+  consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+  consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+  val consumer = new KafkaConsumer[String, String](consumerProps)
+  consumer.subscribe(List("sms-input").asJava)
+
+  val producerProps = new Properties()
+  producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+  producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+  producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+
+  val producer = new KafkaProducer[String, String](producerProps)
+
+  def handleOptIn(sms: SMS): Future[Unit] = {
+    if (sms.message == "START") {
+      db.clientOptedIn.add(sms.sender).map { _ =>
+        sendSMSToTopic(sms, "sms-output")
+        val ackSMS = SMS(sender = optInNumber, recipient = sms.sender, message = "Usługa filtrowania phishingu włączona")
+        sendSMSToTopic(ackSMS, "sms-output")
+      }
+    } else if (sms.message == "STOP") {
+      db.clientOptedIn.remove(sms.sender).map { _ =>
+        sendSMSToTopic(sms, "sms-output")
+        val ackSMS = SMS(sender = optInNumber, recipient = sms.sender, message = "Usługa filtrowania phishingu wyłączona")
+        sendSMSToTopic(ackSMS, "sms-output")
+      }
+    } else {
+      Future.successful()
+    }
+  }
+
+  def handleRegularSMS(sms: SMS): Future[Unit] = {
+    val acceptSMSF = db.clientOptedIn.exists(sms.sender).flatMap {
+      case true => checkIfSMSIsSafe(sms)
+      case false => Future.successful(true)
+    }
+    acceptSMSF.map { acceptSMS =>
+      val topic = if (acceptSMS) {
+        "sms-output"
+      } else {
+        "sms-rejected"
+      }
+      sendSMSToTopic(sms, topic)
+    }
+  }
+
+  def sendSMSToTopic(sms: SMS, topic: String): Future[RecordMetadata] = {
+    println(s"sending $sms to $topic")
+    val outputRecord = new ProducerRecord[String, String]("sms-output", sms.asJson.noSpaces)
+    Future {
+      producer.send(outputRecord).get()
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    val bootstrapServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
-
-    val consumerProps = new Properties()
-    consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "sms-phishing-filter-consumer-group")
-    consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
-    consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
-    consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-
-    val consumer = new KafkaConsumer[String, String](consumerProps)
-    consumer.subscribe(List("sms-input").asJava)
-
-    val producerProps = new Properties()
-    producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-    producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-
-    val producer = new KafkaProducer[String, String](producerProps)
-
     try {
       while (true) {
         val records = consumer.poll(Duration.ofMillis(100))
@@ -102,40 +143,9 @@ object Main extends MyDBProvider {
             case Left(error) => println(s"Error: $error")
             case Right(sms) =>
               if (sms.recipient == optInNumber) {
-                if (sms.message == "START") {
-                  db.clientOptedIn.add(sms.sender).map { _ =>
-                    val outputRecord = new ProducerRecord[String, String]("sms-output", record.key(), record.value())
-                    println(s"sending output record to sms-output")
-                    producer.send(outputRecord)
-                    val ackSMS = SMS(sender = optInNumber, recipient = sms.sender, message = "Usługa filtrowania phishingu włączona")
-                    val ackRecord = new ProducerRecord[String, String]("sms-output", ackSMS.asJson.noSpaces)
-                    producer.send(ackRecord)
-                  }
-                } else if (sms.message == "STOP") {
-                  db.clientOptedIn.remove(sms.sender).map { _ =>
-                    val outputRecord = new ProducerRecord[String, String]("sms-output", record.key(), record.value())
-                    println(s"sending output record to sms-output")
-                    producer.send(outputRecord)
-                    val ackSMS = SMS(sender = optInNumber, recipient = sms.sender, message = "Usługa filtrowania phishingu wyłączona")
-                    val ackRecord = new ProducerRecord[String, String]("sms-output", ackSMS.asJson.noSpaces)
-                    producer.send(ackRecord)
-                  }
-                }
+                handleOptIn(sms)
               } else {
-                val acceptSMSF = db.clientOptedIn.exists(sms.sender).flatMap {
-                  case true => checkIfSMSIsSafe(sms)
-                  case false => Future.successful(true)
-                }
-                acceptSMSF.map { acceptSMS =>
-                  val topic = if (acceptSMS) {
-                    "sms-output"
-                  } else {
-                    "sms-rejected"
-                  }
-                  val outputRecord = new ProducerRecord[String, String](topic, record.key(), record.value())
-                  println(s"sending output record to $topic")
-                  producer.send(outputRecord)
-                }
+                handleRegularSMS(sms)
               }
           }
         }
